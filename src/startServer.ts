30731a21b0ceb7fb72804c91c268ef095c4c2a03
Ludvig Lundgren <hello@ludviglundgren.se>
2018-08-15 16:15:37 +0200

    Replace graphql-yoga with apollo-server 2

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,15 +1,18 @@
-import { GraphQLServer } from "graphql-yoga";
+import * as express from "express";
+import { ApolloServer } from "apollo-server-express";
 import { createConnection } from "typeorm";
 import { genSchema } from "./utils/genSchema";
 
 export const startServer = async () => {
-  const server = new GraphQLServer({ schema: genSchema() as any });
+  const server = new ApolloServer({ schema: genSchema() as any });
+
+  const app = express();
+  server.applyMiddleware({ app });
 
   await createConnection();
-  const app = await server.start({
-    port: process.env.PORT
+  app.listen({ port: process.env.PORT }, () => {
+    console.log(`Server is running on localhost:${process.env.PORT}`);
   });
-  console.log(`Server is running on localhost:${process.env.PORT}`);
 
   return app;
 };
